0a2105aac5b596e7ddc85202ce2126d4b524dde3
Judy Kim <kimjudyh@gmail.com>
2019-11-05 22:43:39 -0800

    Fixed keystone scraper to properly get #open trails

--- scraping/keystone_scraper.py
+++ scraping/keystone_scraper.py
@@ -5,7 +5,7 @@ url = 'https://www.keystoneresort.com/the-mountain/mountain-conditions/terrain-a
 page = requests.get(url)
 
 # check if page retrieved. should output 200
-print(page.status_code)
+print('page status code: ', page.status_code)
 
 # create a BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -17,13 +17,13 @@ trails_summary = soup.find(class_ = 'terrain_summary__tab_main__text')
 trails_summary = soup.find(class_ = 'terrain_summary row')
 
 # look for stuff in <span> tags
-trails_summary_items = trails_summary.find_all('span')
+trails_summary_items = trails_summary.find_all(class_= 'c118__number1--v1')
 
 # get text from span tags
 for item in trails_summary_items:
     print(item.get_text())
-
+'''
 print(trails_summary.prettify())
 print(trails_summary)
 print(trails_summary_items)
-
+'''
