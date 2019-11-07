6cac536c35b1521fe2a2589d7fca36965fabb5d0
Judy Kim <kimjudyh@gmail.com>
2019-11-07 00:38:34 -0800

    Created basic template, changed views.py to use it

--- app_scraping/views.py
+++ app_scraping/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import SkiResort
 
 # Create your views here.
 def index(request):
+    # testing creating a SkiResort object
+    # need to put this code somewhere else because it creates new entries
+    # should be updating existing entries
+    keystone = SkiResort(resort_name="Keystone")
+    keystone.save()
     try:
-        resort_name = SkiResort.resort_name
-        output = resort_name
-        return HttpResponse(output)
-    except:
-
+        output = SkiResort.objects.all()
+        template = loader.get_template('app_scraping/index.html')
+        context= {
+                'resort_list': output,
+                }
+        return HttpResponse(template.render(context, request))
+    except Exception as e:
+        print(e)
         greeting = HttpResponse("This is where Keystone conditions will be posted. Stay tuned.")
         return greeting
