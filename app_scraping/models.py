859f3e7f518c1084865273052416e321e8430d66
Judy Kim <kimjudyh@gmail.com>
2019-11-06 23:24:47 -0800

    Created model SkiResort, modified app view to import model

--- app_scraping/models.py
+++ app_scraping/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SkiResort(models.Model):
+    resort_name = models.CharField(max_length=200)
+    trails_open = models.IntegerField(default=0)
+    lifts_open = models.IntegerField(default=0)
+    acres_open = models.IntegerField(default=0)
--- app_scraping/views.py
+++ app_scraping/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SkiResort
 
 # Create your views here.
 def index(request):
-    greeting = HttpResponse("This is where Keystone conditions will be posted. Stay tuned.")
-    return greeting
+    try:
+        resort_name = SkiResort.resort_name
+        output = resort_name
+        return HttpResponse(output)
+    except:
+
+        greeting = HttpResponse("This is where Keystone conditions will be posted. Stay tuned.")
+        return greeting
