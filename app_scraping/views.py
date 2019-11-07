d62d0e1d2cbe2e75e902efe29c19ae64117b52d2
Judy Kim <kimjudyh@gmail.com>
2019-11-07 01:31:15 -0800

    Fixed and checked import sort order with isort

--- app_scraping/views.py
+++ app_scraping/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from .models import SkiResort
+
 
 # Create your views here.
 def index(request):
