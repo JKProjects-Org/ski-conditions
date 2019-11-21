35a88aa53dd283d50240ddf04e6f33afb7d7d293
Clinton Blackburn <clinton.blackburn@gmail.com>
2019-11-21 13:25:33 -0800

    Proposal: Refactoring of scraping logic

--- ski_conditions/apps/app_scraping/management/commands/do_scraping.py
+++ ski_conditions/apps/app_scraping/management/commands/do_scraping.py
@@ -5,79 +5,97 @@ from django.core.management.base import BaseCommand
 from ski_conditions.apps.app_scraping.models import SkiResort
 
 
+# TODO Use the abc library
+class AbstractScraper:
+    def scrape(self):
+        pass
+
+
+class AbstractVailScraper(AbstractScraper):
+    def _common_scrape(self):
+        page = requests.get(self.url)
+
+        # create a BeautifulSoup object
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        # search for class c118__number1--v1
+        trails_summary = soup.find(class_='terrain_summary row')
+
+        # look for stuff in <span> tags
+        trails_summary_items = trails_summary.find_all(class_='c118__number1--v1')
+
+        # look for trail and lift totals
+        trail_totals = trails_summary.find_all(class_='c118__number2--v1')
+
+        return (trail_totals, trails_summary_items)
+
+
+class KeystoneScraper(AbstractVailScraper):
+    name = 'Keystone'
+    url = 'https://www.keystoneresort.com/the-mountain/mountain-conditions/terrain-and-lift-status.aspx'
+
+    def scrape(self):
+        trail_totals, trails_summary_items = self._common_scrape()
+
+        new_total_trails = int(trail_totals[2].get_text()[2:])
+        new_total_lifts = int(trail_totals[3].get_text()[2:])
+
+        new_acres_open = int(trails_summary_items[0].get_text())
+        new_terrain_percent = int(trails_summary_items[1].get_text())
+        new_trails_open = int(trails_summary_items[2].get_text())
+        new_lifts_open = int(trails_summary_items[3].get_text())
+
+        # TODO Use a struct or other data structure
+        return {
+            'total_trails': new_total_trails,
+            'total_lifts': new_total_lifts,
+        }
+
+
+class HeavenlyScraper(AbstractVailScraper):
+    name = 'Heavenly'
+    url = 'https://www.skiheavenly.com/the-mountain/mountain-conditions/terrain-and-lift-status.aspx'
+
+    def scrape(self):
+        trail_totals, trails_summary_items = self._common_scrape()
+
+        # assign text to variables
+        new_total_trails = int(trail_totals[3].get_text()[2:])
+        new_total_lifts = int(trail_totals[1].get_text()[2:])
+
+        # assign ints to variables
+        new_acres_open = int(trails_summary_items[0].get_text())
+        new_terrain_percent = int(trails_summary_items[2].get_text())
+        new_trails_open = int(trails_summary_items[3].get_text())
+        new_lifts_open = int(trails_summary_items[1].get_text())
+
+        return {
+            'total_trails': new_total_trails,
+            'total_lifts': new_total_lifts,
+        }
+
+
 class Command(BaseCommand):
     help = "Scrapes ski resort website and updates database"
 
     def handle(self, *args, **options):
-        # list of urls
-        url_keystone = \
-                'https://www.keystoneresort.com/the-mountain/mountain-conditions/terrain-and-lift-status.aspx'
-        url_heavenly = \
-                'https://www.skiheavenly.com/the-mountain/mountain-conditions/terrain-and-lift-status.aspx'
-        
-        # place urls in list, loop over them
-        url_list = [url_keystone, url_heavenly]
-        
-        for url in url_list:
-            page = requests.get(url)
+        scrapers = [
+            KeystoneScraper()
+        ]
 
-            # create a BeautifulSoup object
-            soup = BeautifulSoup(page.text, 'html.parser')
+        for scraper in scrapers:
+            name = scraper.name,
+            scraped = scraper.scrape()
 
-            # search for class c118__number1--v1
-            trails_summary = soup.find(class_ = 'terrain_summary row')
-
-            # look for stuff in <span> tags
-            trails_summary_items = trails_summary.find_all(class_= 'c118__number1--v1')
-
-            # look for trail and lift totals
-            trail_totals = trails_summary.find_all(class_='c118__number2--v1')
-
-            if 'heavenly' in url:
-                # assign text to variables
-                new_total_trails = int(trail_totals[3].get_text()[2:])
-                new_total_lifts = int(trail_totals[1].get_text()[2:])
-                # assign ints to variables
-                new_acres_open = int(trails_summary_items[0].get_text())
-                new_terrain_percent = int(trails_summary_items[2].get_text())
-                new_trails_open = int(trails_summary_items[3].get_text())
-                new_lifts_open = int(trails_summary_items[1].get_text())
-
-                name = "Heavenly"
-
-
-            
-            elif 'keystone' in url:
-
-                new_total_trails = int(trail_totals[2].get_text()[2:])
-                new_total_lifts = int(trail_totals[3].get_text()[2:])
-
-                new_acres_open = int(trails_summary_items[0].get_text())
-                new_terrain_percent = int(trails_summary_items[1].get_text())
-                new_trails_open = int(trails_summary_items[2].get_text())
-                new_lifts_open = int(trails_summary_items[3].get_text())
-
-                name = "Keystone"
-
-
-
+            # TODO Expose the rest of the fields
             ski_resort, created = SkiResort.objects.update_or_create(
-                    resort_name = name,
-                    total_trails = new_total_trails,
-                    total_lifts = new_total_lifts,
-                    acres_open = new_acres_open,
-                    terrain_percent = new_terrain_percent,
-                    trails_open = new_trails_open,
-                    lifts_open = new_lifts_open,
-                    )
-
-            '''
-            k = SkiResort.objects.get(**{'resort_name__iexact': name})
-            k.trails_open = trails_open
-            k.lifts_open = lifts_open
-            k.acres_open = acres_open
-            k.terrain_percent = terrain_percent
-            k.save()
-            '''
+                resort_name=name,
+                total_trails=scraped['new_total_trails'],
+                # total_lifts=new_total_lifts,
+                # acres_open=new_acres_open,
+                # terrain_percent=new_terrain_percent,
+                # trails_open=new_trails_open,
+                # lifts_open=new_lifts_open,
+            )
 
         self.stdout.write('SkiResort model updated')
