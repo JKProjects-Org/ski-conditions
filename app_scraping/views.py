b4abc49804889c8c120d53e440fd64e94062ec16
Judy Kim <kimjudyh@gmail.com>
2019-11-21 00:15:50 -0800

    Removed database operations from views.py

--- app_scraping/views.py
+++ app_scraping/views.py
@@ -1,5 +1,6 @@
 import environ
 import psycopg2
+from django.db import connection
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
@@ -8,67 +9,10 @@ from django.utils import timezone
 from .models import SkiResort
 
 
-def ski_scraper(url):
-    '''
-    Inputs:
-    url: string; URL of resort to scrape
-    --------
-    Returns:
-    total_trails: string; total trails in resort
-    total_lifts: string; total lifts in resort
-    acres_open: int; current acres open in resort
-    terrain_percent: int; current percent of terrain open
-    trails_open: int; current number of trails open
-    lifts_open: int; current number of lifts open
-
-    '''
-    from bs4 import BeautifulSoup
-    import requests
-    
-    page = requests.get(url)
-
-    # check if page retrieved. should output 200
-    #print('page status code: ', page.status_code)
-
-    # create a BeautifulSoup object
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    # search for class terrain_summary row
-    trails_summary = soup.find(class_ = 'terrain_summary row')
-
-    # look for open data in class c118__number1--v1
-    trails_summary_items = trails_summary.find_all(class_= 'c118__number1--v1')
-
-    # look for trail and lift totals in class c118__number2--v1
-    trail_totals = trails_summary.find_all(class_='c118__number2--v1')
-
-    if 'heavenly' in url:
-        # assign text to variables
-        total_trails = trail_totals[3].get_text()[2:]
-        total_lifts = trail_totals[1].get_text()[2:]
-
-        # assign ints to variables
-        acres_open = int(trails_summary_items[0].get_text())
-        terrain_percent = int(trails_summary_items[2].get_text())
-        trails_open = int(trails_summary_items[3].get_text())
-        lifts_open = int(trails_summary_items[1].get_text())
-
-    elif 'keystone' in url:
-        # assign text to variables
-        total_trails = trail_totals[2].get_text()[2:]
-        total_lifts = trail_totals[3].get_text()[2:]
-
-        # assign ints to variables
-        acres_open = int(trails_summary_items[0].get_text())
-        terrain_percent = int(trails_summary_items[1].get_text())
-        trails_open = int(trails_summary_items[2].get_text())
-        lifts_open = int(trails_summary_items[3].get_text())
-
-    return total_trails, total_lifts, acres_open, terrain_percent, trails_open, lifts_open
-
 
 # Create your views here.
 def index(request):
+    '''
     # get database name from environment
     env = environ.Env(DEBUG=(bool, False), )
     
@@ -91,7 +35,7 @@ def index(request):
     # insert data into table if resort doesn't exist
     # otherwise, update existing resort data
     cur.execute(
-            f'''INSERT INTO app_scraping_skiresort (resort_name, trails_open, lifts_open, acres_open, 
+            f'INSERT INTO app_scraping_skiresort (resort_name, trails_open, lifts_open, acres_open, 
                 terrain_percent, total_trails, total_lifts) 
                 VALUES ('{resort_name}', {trails_open}, {lifts_open}, {acres_open}, {terrain_percent}, 
                 {total_trails},{total_lifts})
@@ -100,7 +44,7 @@ def index(request):
                         lifts_open = {lifts_open},
                         acres_open = {acres_open},
                         terrain_percent = {terrain_percent}
-                    ''')
+                    ')
 
     db.commit()
         
@@ -114,7 +58,7 @@ def index(request):
     # insert data into table if resort doesn't exist
     # otherwise, update existing resort data
     cur.execute(
-            f'''INSERT INTO app_scraping_skiresort (resort_name, trails_open, lifts_open, acres_open, 
+            f'INSERT INTO app_scraping_skiresort (resort_name, trails_open, lifts_open, acres_open, 
                 terrain_percent, total_trails, total_lifts) 
                 VALUES ('{resort_name}', {trails_open}, {lifts_open}, {acres_open}, {terrain_percent}, 
                 {total_trails},{total_lifts})
@@ -123,12 +67,12 @@ def index(request):
                         lifts_open = {lifts_open},
                         acres_open = {acres_open},
                         terrain_percent = {terrain_percent}
-                    ''')
+                    ')
 
     db.commit()
     db.close()
     
-
+    '''
     # render views
     try:
         output = SkiResort.objects.all()
