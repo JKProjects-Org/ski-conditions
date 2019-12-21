d1d41b11ed3711b85bff8225015d925d12186225
Judy Kim <kimjudyh@gmail.com>
2019-12-20 16:40:37 -0800

    Split test of scraping command into 2 functions

--- ski_conditions/apps/app_scraping/tests/test_commands.py
+++ ski_conditions/apps/app_scraping/tests/test_commands.py
@@ -14,8 +14,13 @@ from ..models import SkiResort
 @responses.activate
 @pytest.mark.django_db()
 def test_scraping():
+    '''
+    test lift and trail number scraping
+    '''
+
     assert SkiResort.objects.count() == 0
 
+    # use saved html instead of accessing live site
     body_Kirkwood = open(os.path.join(os.path.dirname(__file__), 'data', 'kirkwood.html'))
     responses.add(responses.GET, KirkwoodScraper.url, status=200, body=body_Kirkwood.read())
 
@@ -34,10 +39,12 @@ def test_scraping():
     body_Northstar_snow = open(os.path.join(os.path.dirname(__file__), 'data', 'northstar_snow.html'))
     responses.add(responses.GET, NorthstarSnowReport.url, status=200, body=body_Northstar_snow.read())
 
+    # call do_scraping command
     management.call_command('do_scraping')
 
     assert SkiResort.objects.count() == 3
 
+    # assert trail and lift values for Kirkwood, Heavenly, Northstar
     kirkwood = SkiResort.objects.get(resort_name='Kirkwood')
     assert kirkwood.acres_open == 1031
     assert kirkwood.terrain_percent == 45
@@ -45,12 +52,6 @@ def test_scraping():
     assert kirkwood.total_trails == 88
     assert kirkwood.lifts_open == 7
     assert kirkwood.total_lifts == 14
-    assert kirkwood.overnight_snowfall == 1
-    assert kirkwood.twenty_four_hour_snowfall == 2
-    assert kirkwood.forty_eight_hour_snowfall == 2
-    assert kirkwood.seven_day_snowfall == 19
-    assert kirkwood.base_depth == 49
-    assert kirkwood.current_season == 127
 
     heavenly = SkiResort.objects.get(resort_name='Heavenly')
     assert heavenly.acres_open == 22
@@ -59,12 +60,6 @@ def test_scraping():
     assert heavenly.total_trails == 97
     assert heavenly.lifts_open == 4
     assert heavenly.total_lifts == 28
-    assert heavenly.overnight_snowfall == 0
-    assert heavenly.twenty_four_hour_snowfall == 0
-    assert heavenly.forty_eight_hour_snowfall == 0
-    assert heavenly.seven_day_snowfall == 19
-    assert heavenly.base_depth == 30
-    assert heavenly.current_season == 109
 
     northstar = SkiResort.objects.get(resort_name='Northstar')
     assert northstar.acres_open == 1182
@@ -73,6 +68,55 @@ def test_scraping():
     assert northstar.total_trails == 100
     assert northstar.lifts_open == 10
     assert northstar.total_lifts == 20
+
+
+# pylint: disable=no-member
+@responses.activate
+@pytest.mark.django_db()
+def test_snow_report():
+    '''
+    test snow report scraping
+    '''
+
+    # use saved html instead of accessing live site
+    body_Kirkwood = open(os.path.join(os.path.dirname(__file__), 'data', 'kirkwood.html'))
+    responses.add(responses.GET, KirkwoodScraper.url, status=200, body=body_Kirkwood.read())
+
+    body_Heavenly = open(os.path.join(os.path.dirname(__file__), 'data', 'heavenly.html'))
+    responses.add(responses.GET, HeavenlyScraper.url, status=200, body=body_Heavenly.read())
+
+    body_Northstar = open(os.path.join(os.path.dirname(__file__), 'data', 'northstar.html'))
+    responses.add(responses.GET, NorthstarScraper.url, status=200, body=body_Northstar.read())
+
+    body_Kirkwood_snow = open(os.path.join(os.path.dirname(__file__), 'data', 'kirkwood_snow.html'))
+    responses.add(responses.GET, KirkwoodSnowReport.url, status=200, body=body_Kirkwood_snow.read())
+
+    body_Heavenly_snow = open(os.path.join(os.path.dirname(__file__), 'data', 'heavenly_snow.html'))
+    responses.add(responses.GET, HeavenlySnowReport.url, status=200, body=body_Heavenly_snow.read())
+
+    body_Northstar_snow = open(os.path.join(os.path.dirname(__file__), 'data', 'northstar_snow.html'))
+    responses.add(responses.GET, NorthstarSnowReport.url, status=200, body=body_Northstar_snow.read())
+
+    # call do_scraping command
+    management.call_command('do_scraping')
+
+    kirkwood = SkiResort.objects.get(resort_name='Kirkwood')
+    assert kirkwood.overnight_snowfall == 1
+    assert kirkwood.twenty_four_hour_snowfall == 2
+    assert kirkwood.forty_eight_hour_snowfall == 2
+    assert kirkwood.seven_day_snowfall == 19
+    assert kirkwood.base_depth == 49
+    assert kirkwood.current_season == 127
+
+    heavenly = SkiResort.objects.get(resort_name='Heavenly')
+    assert heavenly.overnight_snowfall == 0
+    assert heavenly.twenty_four_hour_snowfall == 0
+    assert heavenly.forty_eight_hour_snowfall == 0
+    assert heavenly.seven_day_snowfall == 19
+    assert heavenly.base_depth == 30
+    assert heavenly.current_season == 109
+
+    northstar = SkiResort.objects.get(resort_name='Northstar')
     assert northstar.overnight_snowfall == 2
     assert northstar.twenty_four_hour_snowfall == 3
     assert northstar.forty_eight_hour_snowfall == 3
