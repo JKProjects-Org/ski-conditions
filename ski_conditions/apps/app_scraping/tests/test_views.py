3d691a9b7af22514c48936040546563d8952803a
Judy Kim <kimjudyh@gmail.com>
2019-12-24 00:00:43 -0800

    Changed app reference from conditions to scraping

--- ski_conditions/apps/app_scraping/tests/test_views.py
+++ ski_conditions/apps/app_scraping/tests/test_views.py
@@ -7,6 +7,6 @@ from .factories import ResortFactory
 @pytest.mark.django_db()
 def test_index_view(client):
     resort = ResortFactory()
-    response = client.get(reverse('conditions:index'))
+    response = client.get(reverse('scraping:index'))
     assert response.status_code == 200
     assert list(response.context['resort_list']) == [resort]
